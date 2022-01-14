ee72e442d21419c828bc1e6604813478cf6c7487
Gustavo <gustavoasf77@gmail.com>
2022-01-14 20:17:17 -0300

    NavBar Update

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,4 +1,5 @@
 import { ReactComponent as GithubIcon } from 'assets/img/github.svg';
+import { Link } from 'react-router-dom';
 import './styles.css';
 
 function Navbar() {
@@ -7,7 +8,9 @@ function Navbar() {
         <header>
             <nav className="container">
                 <div className="dsmovie-nav-content">
+                    <Link to= "/">
                     <h1>DSMovie</h1>
+                    </Link>
                     <a href="https://github.com/SlaypZ">
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
